7b27c06bfdcf6420f23e7aa18046e8c576a76585
PiotrDabrowski232 <piotr.dabrowski2001@o2.pl>
2026-05-17 11:07:43 +0200

    specified gudi for products

--- src/Services/InventoryService/InventoryService.Infrastructure/Data/InventoryDbContextSeed.cs
+++ src/Services/InventoryService/InventoryService.Infrastructure/Data/InventoryDbContextSeed.cs
@@ -16,7 +16,7 @@ public static class InventoryDbContextSeed
             var products = new List<Product>
             {
                 new Product { Id = Guid.Parse("3fa85f64-5717-4562-b3fc-2c963f66afa6"), Name = "Test Product 1", StockQuantity = 100000 },
-                new Product { Id = Guid.NewGuid(), Name = "Test Product 2", StockQuantity = 50000 },
+                new Product { Id = Guid.Parse("6a3d4f1d-d9c2-4045-9008-345c3691cdde"), Name = "Test Product 2", StockQuantity = 50000 },
             };
 
             await context.Products.AddRangeAsync(products);
